c7c6d9886604f906d7e163344a3abe06aa5911cb
Psychic Power <seed58855@gmail.com>
2024-11-29 07:04:51 +0100

    fix: layout locale config

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -52,7 +52,7 @@ export default async function RootLayout(
   // Providing all messages to the client
   // side is the easiest way to get started
   const messages = await getMessages()
-  const layout = await fetchLayoutData()
+  const layout = await fetchLayoutData(locale)
   return (
     <html lang={locale}>
       <body
--- sanity/services/layout/layout.service.ts
+++ sanity/services/layout/layout.service.ts
@@ -1,16 +1,17 @@
+import { routing } from '@/i18n/routing'
 import { fetchSanity, groq } from '@/sanity/fetch'
 
-export const fetchLayoutData = () =>
+export const fetchLayoutData = (locale: string = routing.defaultLocale) =>
   fetchSanity<Sanity.Layout>(
     groq`
       {
-        "header": *[_type == "header"][0] {
+        "header": *[_type == "header" && language == $locale][0] {
             ...
         },
-        "footer": *[_type == "footer"][0] {
+        "footer": *[_type == "footer" && language == $locale][0] {
             ...
         },
         }
     `,
-    { tags: ['faqPage'] },
+    { tags: ['layout'], params: { locale } },
   )
